565d780d4742981c7a3763871d5c16c6cebb5b20
André Fosvold <andrfo@ntnu.no>
2018-10-18 16:21:03 +0200

    started image loading

--- src/citySim/CitySimBuilder.java
+++ src/citySim/CitySimBuilder.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import citySim.environment.Road;
 import repast.simphony.context.Context;
 import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
 import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
@@ -62,7 +63,12 @@ public class CitySimBuilder implements ContextBuilder<Object> {
 		return context;
 	}
 	
-	private void readImage(ContinuousSpace<Object> space, Grid grid) {
+	//black = pixel.r == 0 && pixel.g == 0 && pixel.b == 0;
+    // white = pixel.r > 0.5 && pixel.g > 0.5 && pixel.b > 0.5;
+    // green = pixel.r < 0.5 && pixel.g > 0.5 && pixel.b < 0.5;
+    // red = pixel.r > 0.5 && pixel.g < 0.5 && pixel.b < 0.5;
+	
+	private void readImage(ContinuousSpace<Object> space, Grid grid, Context<Object> context) {
 		BufferedImage img = null;
 		try {
 		    img = ImageIO.read(new File("maps/3-2.png"));
@@ -71,21 +77,39 @@ public class CitySimBuilder implements ContextBuilder<Object> {
 		}
 		width = img.getWidth();
 		height = img.getHeight();
-		
-		//get pixel value
-	    int p = img.getRGB(0,0);
-
-	    //get alpha
-	    int a = (p>>24) & 0xff;
-
-	    //get red
-	    int r = (p>>16) & 0xff;
-
-	    //get green
-	    int g = (p>>8) & 0xff;
-
-	    //get blue
-	    int b = p & 0xff;
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				//get pixel value
+				int p = img.getRGB(0,0);
+				
+				//get alpha
+				int a = (p>>24) & 0xff;
+				
+				//get red
+				int r = (p>>16) & 0xff;
+				
+				//get green
+				int g = (p>>8) & 0xff;
+				
+				//get blue
+				int b = p & 0xff;
+				
+				
+				if(r >= 255 && g >= 255 && b >= 255) {//Nothing
+					continue;
+				}
+				else if(r == 0 && g == 0 && b == 0) {//Road
+					context.add(new Road(location, space, grid))
+				}
+				else if(r <= 100 && g >= 100 && b <= 100) {//Start
+					
+				}
+				else if(r >= 100 && g <= 100 && b <= 100) {//end
+					
+				}
+				
+			}
+		}
 	}
 
 }
